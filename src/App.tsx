a27de877a2efe93851ab1c47921f386782bcb6b5
Dhadhazi <dhadhazi@gmail.com>
2020-10-24 10:14:38 +0200

    Backgrounds added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles/App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainContainer">
+      <header>Our Pricing</header>
+      <div className="priceSwitcherBox">
+        <div>Annually</div> <div>SWITCHER</div>
+        <div>Monthly</div>
+      </div>
+      <div>Price boxes</div>
+      <div className="bg-top">
+        <img src="bg-top.svg" alt="Background waves" />
+      </div>
+      <div className="bg-bottom">
+        <img src="bg-bottom.svg" alt="Background waves" />
+      </div>
     </div>
   );
 }
